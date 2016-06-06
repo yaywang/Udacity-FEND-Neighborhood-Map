3cb5f2eba13ceb9d4585a7257ded43732f7db947
Ye <wangyegp@gmail.com>
2016-06-06 17:25:32 +0300

    Add basic structure to interact with the database

--- src/js/firebaseApp.js
+++ src/js/firebaseApp.js
@@ -1,8 +1,28 @@
 var signInButton = document.getElementById('sign-in-button');
 
+// TODO: Initiate the app's interactions with the database
+function startDatabaseQueries() {
+}
+
+function writeUserData(userId, name, email) {
+	firebase.database().ref('users/' + userId).set({
+		username: name,
+		email: email
+	});
+}
+
 window.addEventListener('load', function() {
 	signInButton.addEventListener('click', function() {
 		var provider = new firebase.auth.GoogleAuthProvider();
 		firebase.auth().signInWithPopup(provider);
 	});
-})
+
+	// Listen for auth state changes
+	// TODO: write the sign-out process
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			writeUserData(user.uid, user.displayName, user.email);
+			startDatabaseQueries();
+		}
+	})
+});
